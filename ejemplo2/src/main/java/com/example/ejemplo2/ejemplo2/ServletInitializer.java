84eed2cafb3407917bd4b70d067958a046177c1a
jf98uni <88836441+jf98uni@users.noreply.github.com>
2022-02-17 14:30:12 -0500

    Se añaden los archivos a github, con avance
    hasta prueba de funcionamiento de la base de datos y creación de la tabla personas

---
+++ ejemplo2/src/main/java/com/example/ejemplo2/ejemplo2/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.ejemplo2.ejemplo2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Ejemplo2Application.class);
+	}
+
+}
---
+++ ejemplo2/src/main/java/com/example/ejemplo2/ejemplo2/models/UsuarioModel.java
@@ -0,0 +1,57 @@
+package com.example.ejemplo2.ejemplo2.models;
+
+import javax.persistence.*;
+
+
+//se dice que la clase es una entidad
+@Entity
+// Se indica que el nombre de la tabla es usuario
+@Table (name = "usuario")
+public class UsuarioModel {
+
+    // se indica que es un id que crese automaticamente, que es unico y no puede ser nulo
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+
+    // constructores de la la clase usuario y del ID
+
+    private Long id;
+
+    private String nombre;
+    private String email;
+    private Integer prioridad;
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPrioridad(Integer prioridad) {
+        this.prioridad = prioridad;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Integer getPrioridad() {
+        return prioridad;
+    }
+}
