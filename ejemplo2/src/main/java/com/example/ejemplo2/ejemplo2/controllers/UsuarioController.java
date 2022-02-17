136613437fd14b0eb6b1cf89e917c237dcaab7ae
jf98uni <jf.botero@uniandes.edu.co>
2022-02-17 14:48:59 -0500

    Se completo UsuatioRepository

---
+++ ejemplo2/src/main/java/com/example/ejemplo2/ejemplo2/controllers/UsuarioController.java
@@ -0,0 +1,2 @@
+package com.example.ejemplo2.ejemplo2.controllers;public class UsuarioController {
+}
---
+++ ejemplo2/src/main/java/com/example/ejemplo2/ejemplo2/services/UsuarioService.java
@@ -0,0 +1,2 @@
+package com.example.ejemplo2.ejemplo2.services;public class UsuarioService {
+}
